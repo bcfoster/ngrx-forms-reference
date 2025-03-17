21be4013f2321fafe68241f251c26d9990e3d73a
Brent Foster <bcfoster@gmail.com>
2025-03-17 00:01:55 -0700

    Add ng-bootstrap alert sample

--- src/app/forms/personal-and-contact-info-form/representative-info-form/representative-info-form.component.ts
+++ src/app/forms/personal-and-contact-info-form/representative-info-form/representative-info-form.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { FormGroupState, NgrxFormsModule } from 'ngrx-forms';
+import { NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
 
 import * as representativeInfo from '../../../state/form/forms/personal-and-contact-info/representative-info.form';
 
 @Component({
   selector: 'representative-info-form',
-  imports: [NgrxFormsModule],
+  imports: [NgbAlertModule, NgrxFormsModule],
   templateUrl: './representative-info-form.component.html',
   styles: ``,
 })
