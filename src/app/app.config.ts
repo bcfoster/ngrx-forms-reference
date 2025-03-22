56903d4aa998846f44764fa42daf00ce5ddcb1cf
Brent Foster <bcfoster@gmail.com>
2025-03-22 01:18:09 -0700

    Scaffold NSwag generated API client

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,18 +1,35 @@
-import { ApplicationConfig, provideZoneChangeDetection, isDevMode } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
+import {
+  ApplicationConfig,
+  provideZoneChangeDetection,
+  isDevMode,
+  makeEnvironmentProviders,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideEffects } from '@ngrx/effects';
 import { provideState, provideStore } from '@ngrx/store';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 import { routes } from './app.routes';
+import { API_BASE_URL } from './services/wrio-api.service';
 import { form } from './state/form';
 import { drafts } from './state/drafts';
 import { DraftsEffects } from './state/drafts/drafts.effects';
 import { FormEffects } from './state/form/form.effects';
 
+const provideApiBaseUrl = () =>
+  makeEnvironmentProviders([
+    {
+      provide: API_BASE_URL,
+      useValue: 'https://localhost:7231',
+    },
+  ]);
+
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideApiBaseUrl(),
     provideEffects([FormEffects, DraftsEffects]),
+    provideHttpClient(),
     provideRouter(routes),
     provideStore(),
     provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
