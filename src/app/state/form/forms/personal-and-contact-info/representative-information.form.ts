802875e470ec121c62745c6ea3649079da5660b2
Brent Foster <bcfoster@gmail.com>
2025-03-23 12:03:55 -0700

    Clear form values representative info form controls are optional

--- src/app/state/form/forms/personal-and-contact-info/representative-information.form.ts
+++ src/app/state/form/forms/personal-and-contact-info/representative-information.form.ts
@@ -1,4 +1,4 @@
-import { FormGroupState, updateGroup } from 'ngrx-forms';
+import { FormGroupState, setValue, updateGroup } from 'ngrx-forms';
 import { minLength, required } from 'ngrx-forms/validation';
 
 import { optional } from '../../../ngrx-forms/optional';
@@ -31,11 +31,11 @@ export const validator = (state: FormGroupState<Form>): FormGroupState<Form> =>
     },
     state.value.reportingForSelf
       ? {
-          firstName: (c) => optional(c),
-          lastName: (c) => optional(c),
-          phoneNumber: (c) => optional(c),
-          relationship: (c) => optional(c),
-          relationshipOther: (c) => optional(c),
+          firstName: (c) => setValue(optional(c), ''),
+          lastName: (c) => setValue(optional(c), ''),
+          phoneNumber: (c) => setValue(optional(c), ''),
+          relationship: (c) => setValue(optional(c), ''),
+          relationshipOther: (c) => setValue(optional(c), ''),
         }
       : {
           firstName: (c) => validate(c, required, minLength(2)),
@@ -43,6 +43,8 @@ export const validator = (state: FormGroupState<Form>): FormGroupState<Form> =>
           phoneNumber: (c) => validate(c, required, minLength(10)),
           relationship: (c) => validate(c, required),
           relationshipOther: (c, f) =>
-            f.value.relationship === 'Other' ? validate(c, required, minLength(4)) : optional(c),
+            f.value.relationship === 'Other'
+              ? validate(c, required, minLength(4))
+              : setValue(optional(c), ''),
         },
   );
