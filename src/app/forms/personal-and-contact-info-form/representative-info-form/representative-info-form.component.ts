a3e95f7e633f2f46d962537aa20b5e8f32cc4738
Brent Foster <bcfoster@gmail.com>
2025-03-25 16:16:18 -0700

    Uninstall Bootstrap

--- src/app/forms/personal-and-contact-info-form/representative-info-form/representative-info-form.component.ts
+++ src/app/forms/personal-and-contact-info-form/representative-info-form/representative-info-form.component.ts
@@ -1,13 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { FormGroupState, NgrxFormsModule } from 'ngrx-forms';
-import { NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
 
 import * as representativeInfo from '../../../state/form/forms/personal-and-contact-info/representative-information.form';
 
 @Component({
   selector: 'representative-info-form',
-  imports: [CommonModule, NgbAlertModule, NgrxFormsModule],
+  imports: [CommonModule, NgrxFormsModule],
   templateUrl: './representative-info-form.component.html',
   styles: ``,
 })
