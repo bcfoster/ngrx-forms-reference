c5bc342fa9cb4bf5ed9e71ea670a97de616186c3
Brent Foster <bcfoster@gmail.com>
2025-03-30 13:39:30 -0700

    Handle a date using three option selects

--- src/app/state/form/forms/personal-and-contact-info/personal-info.form.ts
+++ src/app/state/form/forms/personal-and-contact-info/personal-info.form.ts
@@ -1,4 +1,4 @@
-import { setValue, updateGroup } from 'ngrx-forms';
+import { disable, enable, setValue, updateGroup } from 'ngrx-forms';
 import { validate } from '../../../ngrx-forms/validate';
 import { maxLength, minLength, required } from 'ngrx-forms/validation';
 import { optional } from '../../../ngrx-forms/optional';
@@ -17,6 +17,13 @@ export interface DemographicsForm {
   ancestrey: IndigenousAncestry | '';
 }
 
+export interface DateOfBirthForm {
+  date: string;
+  year: string;
+  month: string;
+  day: string;
+}
+
 export interface Form {
   haveClaimNumber: boolean | null;
   claimNumber: string;
@@ -25,7 +32,7 @@ export interface Form {
   middleName: string;
   lastName: string;
   demographics: DemographicsForm;
-  dateOfBirth: string;
+  dateOfBirth: DateOfBirthForm;
   havePhn: boolean | null;
   phn: string;
   interpreter: boolean | null;
@@ -50,7 +57,12 @@ export const initialFormValue: Form = {
     ancestrey: '',
     associationNation: '',
   },
-  dateOfBirth: '',
+  dateOfBirth: {
+    date: '',
+    year: '',
+    month: '',
+    day: '',
+  },
   havePhn: null,
   phn: '',
   interpreter: null,
@@ -60,34 +72,48 @@ export const initialFormValue: Form = {
   weight: '',
 };
 
-export const validator = updateGroup<Form>({
-  haveClaimNumber: validate(required),
-  claimNumber: (c, f) =>
-    f.value.haveClaimNumber
-      ? validate(c, required, minLength(8), maxLength(8))
-      : optional(setValue(c, '')),
-  legalFirstName: validate(required, minLength(2), maxLength(25)),
-  preferredFirstName: validate(maxLength(25)),
-  middleName: validate(maxLength(10)),
-  lastName: validate(required, minLength(2), maxLength(30)),
-  demographics: updateGroup<DemographicsForm>({
-    sexAtBirth: validate(required),
-    gender: (c) => optional(c),
-    pronouns: (c) => optional(c),
-    indigenousInd: (c) => optional(c),
-    ancestrey: (c) => optional(c),
-    associationNation: (c) => optional(c),
-  }),
-  dateOfBirth: validate(required),
-  havePhn: validate(required),
-  phn: (c, f) =>
-    f.value.havePhn
-      ? validate(c, required, minLength(10), maxLength(10))
-      : optional(setValue(c, '')),
-  interpreter: validate(required),
-  preferredLanguage: (c, f) =>
-    f.value.interpreter ? validate(c, required) : optional(setValue(c, '')),
-  heightFeet: (c) => optional(c),
-  heightInches: (c) => optional(c),
-  weight: (c) => optional(c),
-});
+export const validator = updateGroup<Form>(
+  {
+    dateOfBirth: (c) => {
+      const timestamp = Date.parse(`${c.value.year}-${c.value.month}-${c.value.day}`);
+      const date = isNaN(timestamp) ? '' : new Date(timestamp).toISOString();
+      return updateGroup<DateOfBirthForm>(c, {
+        date: setValue(date),
+        day: (c, f) => (!!f.value.month && !!f.value.year ? enable(c) : disable(c)),
+      });
+    },
+  },
+  {
+    haveClaimNumber: validate(required),
+    claimNumber: (c, f) =>
+      f.value.haveClaimNumber
+        ? validate(c, required, minLength(8), maxLength(8))
+        : optional(setValue(c, '')),
+    legalFirstName: validate(required, minLength(2), maxLength(25)),
+    preferredFirstName: validate(maxLength(25)),
+    middleName: validate(maxLength(10)),
+    lastName: validate(required, minLength(2), maxLength(30)),
+    demographics: updateGroup<DemographicsForm>({
+      sexAtBirth: validate(required),
+      gender: (c) => optional(c),
+      pronouns: (c) => optional(c),
+      indigenousInd: (c) => optional(c),
+      ancestrey: (c) => optional(c),
+      associationNation: (c) => optional(c),
+    }),
+    // dateOfBirth: updateGroup<DateOfBirthForm>({
+    //   date: validate(required),
+    // }),
+    havePhn: validate(required),
+    phn: (c, f) =>
+      f.value.havePhn
+        ? validate(c, required, minLength(10), maxLength(10))
+        : optional(setValue(c, '')),
+    interpreter: validate(required),
+    preferredLanguage: (c, f) =>
+      f.value.interpreter ? validate(c, required) : optional(setValue(c, '')),
+    heightFeet: (c) => optional(c),
+    heightInches: (c) => optional(c),
+    weight: (c) => optional(c),
+  },
+);
